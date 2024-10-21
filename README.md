a98e0c1e098de00cace0ac94d7f69089e8645746
Kristaps <kbebris+git@gmail.com>
2024-10-21 20:52:01 +0300

    looking into pulling sentinel data

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 https://dataspace.copernicus.eu/
 
 probably python?
+
+https://pipx.pypa.io/stable/installation/
+
+set up virtualenv
+```bash
+pipx install virtualenv
+```
+
+create virtual env
+```bash
+python3 -m venv .venv
+```
+
+activate virtual env
+```bash
+source .venv/bin/activate
+```
---
+++ main.py
@@ -0,0 +1,45 @@
+import requests
+
+from settings import token
+
+response = requests.post('https://sh.dataspace.copernicus.eu/api/v1/process',
+  headers={"Authorization" : f"Bearer {token}"},
+  json={
+    "input": {
+        "bounds": {
+            "bbox": [
+                13.822174072265625,
+                45.85080395917834,
+                14.55963134765625,
+                46.29191774991382
+            ]
+        },
+        "data": [{
+            "type": "sentinel-2-l2a"
+        }]
+    },
+    "evalscript": """
+    //VERSION=3
+
+    function setup() {
+      return {
+        input: ["B02", "B03", "B04"],
+        output: {
+          bands: 3
+        }
+      };
+    }
+
+    function evaluatePixel(
+      sample,
+      scenes,
+      inputMetadata,
+      customData,
+      outputMetadata
+    ) {
+      return [2.5 * sample.B04, 2.5 * sample.B03, 2.5 * sample.B02];
+    }
+    """
+})
+
+print(response)
