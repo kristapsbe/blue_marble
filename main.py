d17219f68125eeb423dc7762c90b73079cbfa7d4
Kristaps <kbebris+git@gmail.com>
2024-10-22 18:02:49 +0300

    messing around with demo code

--- main.py
+++ main.py
@@ -3,45 +3,59 @@ import requests
 
 
 token = os.getenv("ACCESS_TOKEN")
+base_url = "https://sh.dataspace.copernicus.eu/api/v1/process"
 
-response = requests.post('https://sh.dataspace.copernicus.eu/api/v1/process',
-  headers={"Authorization" : f"Bearer {token}"},
-  json={
+evalscript = """
+//VERSION=3
+function setup() {
+    return {
+        input: ["B02", "B03", "B04"],
+        output: {
+            bands: 3,
+            sampleType: "AUTO", // default value - scales the output values from [0,1] to [0,255].
+        },
+    }
+}
+
+function evaluatePixel(sample) {
+    return [2.5 * sample.B04, 2.5 * sample.B03, 2.5 * sample.B02]
+}
+"""
+
+request = {
     "input": {
         "bounds": {
+            "properties": {"crs": "http://www.opengis.net/def/crs/OGC/1.3/CRS84"},
             "bbox": [
                 13.822174072265625,
                 45.85080395917834,
                 14.55963134765625,
-                46.29191774991382
-            ]
+                46.29191774991382,
+            ],
         },
-        "data": [{
-            "type": "sentinel-2-l2a"
-        }]
+        "data": [
+            {
+                "type": "sentinel-2-l2a",
+                "dataFilter": {
+                    "timeRange": {
+                        "from": "2022-10-01T00:00:00Z",
+                        "to": "2022-10-31T00:00:00Z",
+                    }
+                },
+            }
+        ],
     },
-    "evalscript": """
-    //VERSION=3
+    "output": {
+        "width": 512,
+        "height": 512,
+    },
+    "evalscript": evalscript,
+}
 
-    function setup() {
-      return {
-        input: ["B02", "B03", "B04"],
-        output: {
-          bands: 3
-        }
-      };
-    }
-
-    function evaluatePixel(
-      sample,
-      scenes,
-      inputMetadata,
-      customData,
-      outputMetadata
-    ) {
-      return [2.5 * sample.B04, 2.5 * sample.B03, 2.5 * sample.B02];
-    }
-    """
-})
+response = requests.post(
+    base_url,
+    headers={"Authorization" : f"Bearer {token}"},
+    json=request
+)
 
 print(response.content)
